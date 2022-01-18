782a75788ffae35c86e0f6d9d9370075df59a9f1
sudddy <suddpro@gmail.com>
2022-01-18 17:51:56 +1300

    installed npm packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,9 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <h1 className="text-3xl font-bold underline">
+  Hello world!
+</h1>
 }
 
 export default MyApp
