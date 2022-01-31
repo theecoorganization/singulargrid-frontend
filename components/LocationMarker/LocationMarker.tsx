2aeade89cf569d41f6024ff55b2c833c360edce4
sudddy <suddpro@gmail.com>
2022-01-31 15:58:05 +1300

    Navigation and button styles

--- components/LocationMarker/LocationMarker.tsx
+++ components/LocationMarker/LocationMarker.tsx
@@ -1,8 +1,13 @@
 
 import { Fragment, useState } from "react";
 import { InfoWindow, Marker } from "react-google-maps";
+import { useRouter } from "next/dist/client/router";
+
+
+
 
 const LocationMarker = ({markers}) => {
+    const router = useRouter();
     const [activeMarker, setActiveMarker] = useState(null);
     const handleActiveMarker = (marker) => {
         if (marker === activeMarker) {
@@ -10,6 +15,10 @@ const LocationMarker = ({markers}) => {
         }
         setActiveMarker(marker);
       };
+
+    const addDetails = () => {
+      router.push('/admin/new');
+    }
 
     return (
         <Fragment>
@@ -21,8 +30,8 @@ const LocationMarker = ({markers}) => {
               > 
             {activeMarker === index ?  (<InfoWindow key={index} onCloseClick={() => handleActiveMarker(null)}>
                 <div>
-                    <h2>Latitude: </h2>
-                    <h2>Longitude: </h2>
+                   <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={addDetails} name="remove marker"> Remove Marker</button> <br />
+                   <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"  name="Add details"> Add Details </button>
             </div>
             </InfoWindow>) : null}
             </Marker>
