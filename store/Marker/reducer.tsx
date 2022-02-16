e5e69adb3c8cd3ae42eae032593377c018de195f
sudddy <suddpro@gmail.com>
2022-02-16 15:55:51 +1300

    Fixed the multiple marker issue

--- store/Marker/reducer.tsx
+++ store/Marker/reducer.tsx
@@ -4,7 +4,7 @@ import { markerActionTypes } from './action'
 export default function reducer(state = [], action) {
   switch (action.type) {
     case markerActionTypes.ADD:
-        return [...state, action.values]    
+        return [action.values]    
     case markerActionTypes.DELETE:
         return [...state.filter(item => item != action.values)]
         
